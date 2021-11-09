96d8ebfd79fdbbcb27dcc04819cf8f9c5ae8a95e
Iewandowski <Caroline.Rodrigues99@edu.pucrs.br>
2021-11-08 21:33:55 -0300

    commit inicial

---
+++ tagcloud.c
@@ -0,0 +1,17 @@
+/*    */
+
+#define PROGRAM_NAME "tagtree"
+
+#define AUTHORS \
+  proper_name ("Carol Lewandowski"), \
+  proper_name ("Leticia Flores")
+
+void
+usage ()
+{
+}
+
+int
+main (int argc, char **argv)
+{
+}
