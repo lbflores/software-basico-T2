f1e26242f34cddb91958d0ceb741ec6ca63adb13
lbflores <leticiabrasilflores@gmail.com>
2021-11-17 22:06:52 -0300

    menu e usage

--- tagcloud.c
+++ tagcloud.c
@@ -28,22 +28,38 @@
   proper_name ("Carol Lewandowski"), \
   proper_name ("Leticia Flores")
 
+
 void
 usage ()
 {
+    printf("Texto usage\n\
+            - Op l -  O tag cloud generator gera nuvem de palavras a partir de um aquivo txt.\n\
+            - Op i -  Realiza a leitura de palavras em um arquivo txt. Informar nome do arquivo. \n\
+            - Op t -  Exibe o resultado em arquivo txt. Palavras e frequencia. \n\
+            - Op g -  Exibe o resultado no navegador como Tag Cloud.\n\
+            - Op s -  Encerra o programa. \n");
+
 }
 
-int
-main (int argc, char **argv)
+/* Chamada da função principal */
+void 
+tcg()
+{ 
+} 
+
+/* Chamada da função de leitura */
+void 
+lerArquivo()
 {
-char in_name[80];
+    char in_name[80];
     FILE *in_file;
     char word[50];
 
-    printf("Enter file name:\n");
-    scanf("%s", in_name);
+    //printf("Enter file name:\n");  //descomentar depois
+    //scanf("%s", in_name);
 
-    in_file = fopen(in_name, "r");
+    //in_file = fopen("in_name", "r");
+    in_file = fopen("lorem-ipsum.txt", "r");
 
     if (in_file == NULL)
         printf("Can't open %s for reading.\n", in_name);
@@ -55,5 +71,55 @@ char in_name[80];
         }
         fclose(in_file);
     }
+}
+
+int
+main (int argc, char **argv)
+{
+     /* Opção do usuário */
+    char q;
+
+    do{
+        printf("\n\nTAG Cloud:\
+        \nu -> para acessar o usage; \
+        \ni -> para abrir e ler arquivo;\
+        \nl -> para executar a funcao principal;\
+        \nt -> para visulizar saida em texto;\
+        \ng -> para visaulizar saida grafica no navegador\
+        \ns -> para sair \n:");
+
+        fflush(stdin);	     
+        scanf("%c", &q);     /* Le a opcao do usuario */
+        fflush(stdin);       
+
+  switch(q) {
+            case 'u': case 'U':		/* Exibe o usage */
+                usage(); 
+                break;
+
+            case 'i': case 'I':		/* Abertura e leitura do arquivo */
+                lerArquivo();
+            	break;   
+
+            case 'l': case 'L':		/* função principal do programa */
+                tcg();
+                break;    
+
+            case 't': case 'T':		/* função para saida em txt */
+               
+                break;    
+
+            case 'g': case 'G':		/* função para saida no navegador */
+               
+                break;                  
+
+            case 's': case 'S':		/* Sair do programa */
+            	break;
+
+            default:
+            	printf("\n Opcao invalida");
+        }
+    } while ((q != 's') && (q != 'S') );
+
     return 0;
 }
